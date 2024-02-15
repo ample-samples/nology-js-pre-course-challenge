ae0236a796bacffdd2cb2568c3681f19040ed223
Todd <todd.griffin61@outlook.com>
2024-02-15 15:16:31 +0000

    adjusted formatting

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function isNumberPositive(num) {
-	return num >=0;
+	return num >= 0;
 }
 
 function convertDaysToAge(days) {
-	return Math.floor(days/365);
+	return Math.floor(days / 365);
 }
 
 function getLargestNumber(num1, num2, num3) {
@@ -20,20 +20,18 @@ function allNumbersPositive(numbers) {
 }
 
 console.log(
+	isNumberPositive(-1), // returns false
+	isNumberPositive(10), // returns true
 
-    isNumberPositive(-1), // returns false
-    isNumberPositive(10), // returns true
+	convertDaysToAge(3650), // returns 10
+	convertDaysToAge(6570), // returns 18
 
-    convertDaysToAge(3650), // returns 10
-    convertDaysToAge(6570), // returns 18
+	getLargestNumber(2, 1, 4), // returns 4
+	getLargestNumber(6, 2, 3), // returns 6
 
-    getLargestNumber(2, 1, 4), // returns 4
-    getLargestNumber(6, 2, 3), // returns 6
+	getLastName(["Charlie", "Rob", "Andy"]), // returns “Andy"
+	getLastName(["Ash", "Stu"]), // returns "Stu"
 
-    getLastName(["Charlie", "Rob", "Andy"]), // returns “Andy"
-    getLastName(["Ash", "Stu"]), // returns "Stu"
-
-    allNumbersPositive([2, 4, 5]), // returns true
-    allNumbersPositive([-5, 4, 6]) // returns false
-
+	allNumbersPositive([2, 4, 5]), // returns true
+	allNumbersPositive([-5, 4, 6]) // returns false
 )
