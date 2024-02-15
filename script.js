4c0fa5b22d6cab3fa0c80b6b326e3f2b03f99cf3
Todd <todd.griffin61@outlook.com>
2024-02-15 14:55:31 +0000

    added isNumberPositive

---
+++ script.js
@@ -0,0 +1,22 @@
+function isNumberPositive(num) {
+	return num >=0;
+}
+
+console.log(
+
+    isNumberPositive(-1), // returns false
+    isNumberPositive(10), // returns true
+
+    // convertDaysToAge(3650), // returns 10
+    // convertDaysToAge(6570), // returns 18
+    //
+    // getLargestNumber(2, 1, 4), // returns 4
+    // getLargestNumber(6, 2, 3), // returns 6
+    //
+    // getLastName(["Charlie", "Rob", "Andy"]), // returns “Andy"
+    // getLastName(["Ash", "Stu"]), // returns "Stu"
+    //
+    // allNumbersPositive([2, 4, 5]), // returns true
+    // allNumbersPositive([-5, 4, 6]) // returns false
+
+)
